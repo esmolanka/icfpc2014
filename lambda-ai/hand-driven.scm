8294c2f47755ccb39e7a9dbe7aaffa027192f67c
Anton Dubovik <gunner.kade@gmail.com>
2014-07-27 23:26:50 +0300

    Finally working version of hand-driven AI...

--- lambda-ai/hand-driven.scm
+++ lambda-ai/hand-driven.scm
@@ -39,20 +39,31 @@
 
 ;; Algo itself
 
-(define (next-right-dir dir)
+(define (next-left-dir dir)
   (cond ((== dir 0) 3)
         (#t (- dir 1))))
 
-(define (next-left-dir dir)
+(define (next-right-dir dir)
   (cond ((== dir 3) 0)
         (#t (+ dir 1))))
 
 ;; hand = 1 (right hand) or 0 (left hand)
-(define (hand-driven prev-dir loc hand wmap)
-  (letrec ((next-loc (get-loc-in-direction loc prev-dir))
-           (rotated-dir (cond ((== hand 1) (next-right-dir prev-dir))
-                              ((== hand 0) (next-left-dir  prev-dir))))
 
-           (next-dir (cond ((non-blocked? wmap next-loc) prev-dir)
-                           (#t (hand-driven next-dir loc hand wmap)))))
-    next-dir))
+(define (hand-driven prev-dir loc hand wmap)
+  (let* ((rot-dir1 (cond ((== hand 1) (next-right-dir prev-dir))
+                         ((== hand 0) (next-left-dir  prev-dir))))
+         (opp-dir  (cond ((== hand 1) (next-right-dir rot-dir1))
+                         ((== hand 0) (next-left-dir  rot-dir1))))
+         (rot-dir2 (cond ((== hand 1) (next-right-dir opp-dir))
+                         ((== hand 0) (next-left-dir  opp-dir))))
+
+         (rot-loc1 (get-loc-in-direction loc rot-dir1))
+         (opp-loc  (get-loc-in-direction loc opp-dir ))
+         (rot-loc2 (get-loc-in-direction loc rot-dir2))
+         (prev-loc (get-loc-in-direction loc prev-dir))
+
+         (next-dir (cond ((non-blocked? wmap rot-loc1) rot-dir1)
+                         ((non-blocked? wmap prev-loc) prev-dir)
+                         ((non-blocked? wmap rot-loc2) rot-dir2)
+                         (#t opp-dir))))
+    (debug-it next-dir)))
