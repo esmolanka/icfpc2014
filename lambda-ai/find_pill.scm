0626ee5ce5004a2f318341dd5e0a79113216c37c
Ivan Veselov <veselov@gmail.com>
2014-07-27 23:06:49 +0300

    AI looks for more pills if found the first

--- lambda-ai/find_pill.scm
+++ lambda-ai/find_pill.scm
@@ -1,21 +1,32 @@
+(define +done+ 42)
+
 (define (main world undoc)
   (cons (power-pills-positions (world-map world)) step))
 
+(define (debug-tuple t)
+  (debug (car t))
+  (debug (cdr t)))
+
 (define (step state world)
+  (debug-tuple (first state))
   (let* ((wmap (world-map world))
          (lman (lm-status world))
          (loc (lm-location lman))
          (dir (dir-to-target loc (first state))))
-    (cons state dir)))
+    (if (== dir +done+)
+        (cons (cdr state) +up+)
+        (cons state dir))))
 
 (define (dir-to-target from to)
   (let* ((dx (- (car to) (car from)))
          (dy (- (cdr to) (cdr from))))
-    (if (> (abs dy) (abs dx))
-        ;; up or down
-        (if (> dy 0) +up+ +down+)
-        ;; left or right
-        (if (> dx 0) +right+ +left+))))
+    (if (and (== dx 0) (== dy 0))
+        +done+
+        (if (> (abs dy) (abs dx))
+            ;; up or down
+            (if (> dy 0) +up+ +down+)
+            ;; left or right
+            (if (> dx 0) +right+ +left+)))))
 
 (define (filter-wmap pred? wmap)
   (let* ((size (map-size wmap))
@@ -29,9 +40,7 @@
        (concat-map
         (lambda (colix-cell)
           (if (pred? (cdr colix-cell))
-              (begin
-                (debug 1)
-                (list (cons (car colix-cell) (car rowix-row))))
+              (list (cons (car colix-cell) (car rowix-row)))
               +nil+))
         (zip ys (cdr rowix-row))))
      (zip xs wmap))))
