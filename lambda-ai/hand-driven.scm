90fa25e2dd968f33877f70ef9b69e6a03a891669
Anton Dubovik <gunner.kade@gmail.com>
2014-07-27 22:13:13 +0300

    Fixed AI compilcation error.

--- lambda-ai/hand-driven.scm
+++ lambda-ai/hand-driven.scm
@@ -49,10 +49,10 @@
 
 ;; hand = 1 (right hand) or 0 (left hand)
 (define (hand-driven prev-dir loc hand wmap)
-  (let* ((next-loc (get-loc-in-direction loc prev-dir))
-         (rotated-dir (cond ((== hand 1) (next-right-dir prev-dir))
-                            ((== hand 0) (next-left-dir  prev-dir))))
+  (letrec ((next-loc (get-loc-in-direction loc prev-dir))
+           (rotated-dir (cond ((== hand 1) (next-right-dir prev-dir))
+                              ((== hand 0) (next-left-dir  prev-dir))))
 
-         (next-dir (cond ((non-blocked? wmap next-loc) prev-dir)
-                         (#t (hand-driven next-dir loc hand wmap)))))
+           (next-dir (cond ((non-blocked? wmap next-loc) prev-dir)
+                           (#t (hand-driven next-dir loc hand wmap)))))
     next-dir))
